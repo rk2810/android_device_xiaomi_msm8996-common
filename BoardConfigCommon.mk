d39bd89d8adfdb4671a91a10df40d0ab40c06a55
norman_liu <norman_liu@htc.com>
2016-03-19 02:16:05 +0800

    EAS: gemini: add dumpstate static HAL
    BUG: 27799254
    
    Change-Id: I70595f64283b06e4ce100caec64aeb76ec6c25d7
    Signed-off-by: norman_liu <norman_liu@htc.com>
    Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,6 +182,9 @@ TARGET_HAS_NO_WIFI_STATS := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 TARGET_USES_INTERACTION_BOOST := true
 
+BOARD_HAL_STATIC_LIBRARIES := libdumpstate.gemini
+
+
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_USE_SDCLANG := true
