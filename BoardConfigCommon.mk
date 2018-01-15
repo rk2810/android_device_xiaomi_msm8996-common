4ad09eb459bf6fa2e020b5d54f03a7b4923b18b6
rk2810 <rohan.kumquat@gmail.com>
2018-01-15 22:06:59 +0530

    Use linaro 7.2 toolchain for kernel - Set it to permissive for now.
    Signed-off-by: rk2810 <rohan.kumquat@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -45,7 +45,6 @@ TARGET_NO_BOOTLOADER := true
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff boot_cpus=0,1,2,3
 BOARD_KERNEL_CMDLINE += bob_and_vegana=0
-# BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -53,7 +52,8 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8996
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/linaro7.2/bin
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8996
