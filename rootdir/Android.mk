e21815ebb9a125591308bc9faa91331fb0cdb42c
MOVZX <movzx@yahoo.com>
2018-02-14 04:05:28 +0300

    Update Darkness Kernel Profile
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -67,7 +67,7 @@ LOCAL_MODULE		:= init.darkness.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.darkness.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -75,7 +75,7 @@ LOCAL_MODULE		:= init.spectrum.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.spectrum.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
