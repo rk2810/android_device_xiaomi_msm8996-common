9823d517db942af0dfb9a474e3cb65648b0e1224
rk2810 <rohan.kumquat@gmail.com>
2018-05-01 06:02:43 +0000

    Revert "msm8996-common: Drop USB HAL service"
    This reverts commit 0fc3cbb0a3091d326574ec0b65e60f34894d9324.

--- msm8996.mk
+++ msm8996.mk
@@ -358,6 +358,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-sp
